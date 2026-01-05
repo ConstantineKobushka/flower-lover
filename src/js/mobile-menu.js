88a06b85fc453e0754f427c3698f2194fc535a1f
Constantine Kobushka <kobushkaconstantine@gmail.com>
2026-01-05 15:06:19 +0200

    add mobile menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,27 @@
+const burgerBtn = document.querySelector('.header-burger-menu');
+const mobileCloseBtn = document.querySelector('.mobile-close-btn');
+const mobileOverlay = document.querySelector('.mobile-overlay');
+const mobileMenu = document.querySelector('.mobile-inner');
+
+burgerBtn.addEventListener('click', openMobileMenuHandler);
+mobileCloseBtn.addEventListener('click', closeMobileMenuHandler);
+mobileOverlay.addEventListener('click', closeMobileMenuHandler);
+mobileMenu.addEventListener('click', scrollToSection);
+
+function openMobileMenuHandler() {
+  mobileOverlay.classList.add('is-open');
+  document.body.classList.add('no-scroll');
+}
+
+function closeMobileMenuHandler() {
+  mobileOverlay.classList.remove('is-open');
+  document.body.classList.remove('no-scroll');
+}
+
+function scrollToSection(e) {
+  e.stopPropagation();
+  console.log(e.target);
+  if (e.target.closest('.mobile-menu-link')) {
+    closeMobileMenuHandler();
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 import './js/mobile-menu';
 import './js/catalog';
 import './js/order';
-import './js/scroll-up';
